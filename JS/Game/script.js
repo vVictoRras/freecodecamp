597fdc572643fa2108cf0f4f52ec5dd74dcb0efd
Victor <you@example.com>
2024-11-14 21:05:11 +0200

    Js Game

---
+++ JS/Game/script.js
@@ -0,0 +1,12 @@
+/*You can use Math.random() and Math.floor() to help you get a random whole number.
+This will represent the index number for the options array.
+You can use the random index to access the option from the options array.*/
+
+function getRandomComputerResult() {
+    const options = ["Rock", "Paper", "Scissors"];
+    const rndInt = Math.floor(Math.random() * 3)
+    return options[rndInt];
+
+}
+
+console.log(getRandomComputerResult());
